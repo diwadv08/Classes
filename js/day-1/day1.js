d0da5acdab77ce92c71aa96201263dce59ccb5de
Diwakar <you@example.com>
2024-12-11 12:06:31 +0530

    js

---
+++ js/day-1/day1.js
@@ -0,0 +1,13 @@
+document.write(`<h1>10+5</h1>
+    <h1>10+5</h1>
+    <h1>10+5</h1>
+    <h1>10+5</h1>`);
+
+console.log(10)
+console.error(10)
+console.warn(10)
+
+
+alert(10)
+prompt('Enter name')
+confirm('Are you sure!')
