be059af5252e95e915b66536907bfa829c772984
Diwakar <you@example.com>
2024-12-12 12:21:04 +0530

    Datatypes & Variables

---
+++ js/day-2/datatypes.js
@@ -0,0 +1,16 @@
+var a='10';  //string
+var b=10;   //number
+var c=false;  //boolean
+var d=new Date();  //Object
+var e;  //undefined
+var n=null; //object
+
+// alert(typeof(d))
+alert(typeof(n))
+// alert(typeof(e))
+// alert(typeof(a))
+// alert(typeof(b))
+
+// innerHTML
+// typeof
+// Number
---
+++ js/day-2/variable.js
@@ -0,0 +1,20 @@
+//var ,let ,const
+'use strict'
+// var a=4;
+// var b=10;
+document.write('<h1>The sum of '+a+' and '+b+' is '+(a+b)+ '</h1>');
+document.write(`<h1>The Sum of ${a} and ${b} is ${a+b}</h1>`);
+
+// const a=10; // cannot be changed
+
+// a=20;
+// alert(a)
+// a=30;
+// var a;  //20
+
+// a=20;
+// alert(a)
+// a=30;
+// let a;  //Error!
+
+// const=20  Error
