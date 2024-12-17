1b1be40eaa3bae9d8a91be18567a1a934087e380
Diwakar <you@example.com>
2024-12-17 12:05:31 +0530

    string method

--- js/day-3/js.js
+++ js/day-3/js.js
@@ -30,31 +30,58 @@
 // console.log(card_details.price.new_price)
 
 
-const obj={
-    id: 1,
-    name: "Leanne Graham",
-    username: "Bret",
-    email: "Sincere@april.biz",
-    address: {
-        street: "Kulas Light",
-        suite: "Apt. 556",
-        city: "Gwenborough",
-        zipcode: "92998-3874",
-        geo: {
-            lat: "-37.3159",
-            lng: "81.1496"
-        }
-    },
-    phone: "1-770-736-8031 x56442",
-    website: "hildegard.org",
-    company: {
-        name: "Romaguera-Crona",
-        catchPhrase: "Multi-layered client-server neural-net",
-        bs: "harness real-time e-markets"
-    }
-  }
+// const obj={
+//     id: 1,
+//     icons:'circle',
+//     name: "Leanne Graham",
+//     username: "Bret",
+//     email: "Sincere@april.biz",
+//     address: {
+//         street: "Kulas Light",
+//         suite: "Apt. 556",
+//         city: "Gwenborough",
+//         zipcode: "92998-3874",
+//         geo: {
+//             lat: "-37.3159",
+//             lng: "81.1496"
+//         }
+//     },
+//     phone: "1-770-736-8031 x56442",
+//     website: "hildegard.org",
+//     company: {
+//         name: "Romaguera-Crona",
+//         catchPhrase: "Multi-layered client-server neural-net",
+//         bs: "harness real-time e-markets"
+//     }
+//   }
 
 // console.log(a);
-var b=[10,20,30];
-console.log(b);
+// var b=[10,20,30];
+// console.log(b);
+
+
+// document.write(`<h1><i class='fa fa-${obj.icons}'></i></h1>`)
+
+
+
+// variables New
+
+// var a=[1,2,3,4];
+// a[2]=10;
+// a[2]=40;
+// a[2]=50;
+
+var a={
+    name1:"Hari",
+    name1:"Arun",
+    name1:"Ajay",
+}
+
+a.name1='Harish';
+a.age=22;
+a['name1']="Siva";
+a['age']=10;
+
+
+console.log(a);
 
---
+++ js/day-4/string_method.js
@@ -0,0 +1,29 @@
+// var arr=[1,2,3,4];
+var a="Hello World";
+// alert(a.length)  //11
+// alert(a.indexOf('l'))  //2     
+// alert(a.lastIndexOf('l'))  //9
+// alert(a.charAt(1)) //e
+// alert(a.charCodeAt(2))  //108
+
+// alert(a.substr(1,3))  //ell
+
+// alert(a.includes('inge')) //false
+// alert(a.startsWith('E'))  //false
+// alert(a.endsWith('orld'))  //true
+
+// alert(a.replace('e','-'))  //H-llo World
+// alert(a.replaceAll('e','-'))  //H-llo World
+
+// alert(a.toUpperCase())  HELLO WORLD
+// alert(a.toLowerCase())  hello world
+// console.log(a); Hello World 
+// console.log(a.split('e')); ['h','llo World']
+// console.log(arr.toString());   [1,2,3,4]="1,2,3,4"
+// alert('hi'+a+'hi')
+// alert('hi'+a.trimStart()+'hi')
+// alert('hi'+a.trimEnd()+'hi')
+// alert('hi'+a.trim()+'hi')
+
+
+
