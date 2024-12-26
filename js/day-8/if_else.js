dc21e8df9f265f294399f16c4a5e65bc62a27753
Diwakar <you@example.com>
2024-12-26 11:07:41 +0530

    new

--- js/day-8/if_else.js
+++ js/day-8/if_else.js
@@ -1,26 +1,49 @@
-var a=8;
-if(isFinite(a)){
-    if(a%2===0){
-        alert(a+'-even')
-    }
-    else if(a%2!==0){
-        alert(a+'-odd')
-    }
-}
-else{
-    alert(a+'-not a number')
-}
+// var a=8;
+// if(isFinite(a)){
+//     if(a%2===0){
+//         alert(a+'-even')
+//     }
+//     else if(a%2!==0){
+//         alert(a+'-odd')
+//     }
+// }
+// else{
+//     alert(a+'-not a number')
+// }
 
 
-if(isNaN(a)){
-    alert(a+'-not a number')
-}
-else{
-    if(a%2===0){
-        alert(a+'-even')
-    }
-    else if(a%2!==0){
-        alert(a+'-odd')
-    }
-}
+// if(isNaN(a)){
+//     alert(a+'-not a number')
+// }
+// else{
+//     if(a%2===0){
+//         alert(a+'-even')
+//     }
+//     else if(a%2!==0){
+//         alert(a+'-odd')
+//     }
+// }
 
+var a=50;
+//5,10,15
+//10,15,20,25
+
+// if(a%2==0){
+//     alert(a+' is % by 2')
+//     if(a%5==0){
+//         alert(a+' is % by 5')
+//         if(a%10==0){
+//             alert(a+' is % by 10')
+//         }
+//         else{
+//             alert(a+' is % by 10')
+//         }
+//     }
+//     else{
+//         alert(a+' is not % by 5')
+//     }
+// }
+
+// else{
+//     alert(a+' is not % by 2')
+// }
---
+++ js/day-9/for.js
@@ -0,0 +1,17 @@
+
+for(let i=1;i<=10;i++){
+    if(i%2==0){
+        document.write(i+'-even<br>')
+    }
+    else{
+        document.write(i+'-odd<br>')
+        
+    }
+}
+
+
+// greater than --
+// lesser than ++
+
+
+
