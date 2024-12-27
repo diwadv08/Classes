f81306ebfc2765be6e37535a63efcff09fcdd16e
Diwakar <you@example.com>
2024-12-27 15:35:21 +0530

    27-12-2024

---
+++ js/day-11/function.js
@@ -0,0 +1,93 @@
+// (function c1(){
+//     alert('a')
+// }())
+
+// function c1(){
+//     alert('a')
+// }
+
+// Global Variable
+
+// function math1(a=1,b=1){
+//     document.write(`<h1>The sum of <span>${a}</span> and <span>${b}</span> is <span>${a+b}</span></h1>`)
+//     document.write(`<h1>The diffrence of <span>${a}</span> and <span>${b}</span> is <span>${a-b}</span></h1>`)
+//     document.write(`<h1>The multiplication of <span>${a}</span> and <span>${b}</span> is <span>${a*b}</span></h1>`)
+//     document.write(`<h1>The division of <span>${a}</span> and <span>${b}</span> is <span>${a/b}</span></h1>`)
+//     document.write('<hr>')
+// }
+
+
+// math1(4)
+// math1()
+// math1(10,2)
+
+
+
+// function c1(){
+//     return 10;
+// }
+
+// var a=10;  //undefined
+
+
+// var b=c1();
+// alert(b)
+
+
+// const F1=(w=0)=>{
+//     alert(w)
+// }
+
+// F1()
+// F1(1)
+// F1(2)
+// F1(3)
+
+
+
+// const F1=function(){
+//     alert('a')
+
+// }
+
+
+// const F1=()=>{
+//     alert('a')
+    
+// }
+
+// const Change=(e)=>{
+//     var btnText=e.innerHTML;
+//     if(btnText==='Login'){
+//         e.style.color='red';
+//         e.style.background='black';
+//         e.innerHTML='Logout';
+//     }
+//     else if(btnText==='Logout'){
+//         e.innerHTML='Login';
+//         e.style.color='black';
+//         e.style.background='red';
+//     }
+// }
+
+// const Hide=(e)=>{
+//     if(e.className=='fa fa-eye-slash i1'){
+//         e.className='fa fa-eye i1'
+//     }
+//     else if(e.className=='fa fa-eye i1'){
+//         e.className='fa fa-eye-slash i1'
+//     }
+//     console.log(e);
+// }
+
+
+// function F1(a,b,c){
+//     return eval(a+b+c);
+// }
+// alert(F1(8,'-',5))
+
+// let a=10
+// function MM(a=0){
+//     alert(a)
+// }
+// MM()
