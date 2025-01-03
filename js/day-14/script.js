68c76caed2a2fd57f6b9e0b9a7c7d56bc3835b7f
Diwakar <you@example.com>
2025-01-03 11:04:26 +0530

    02-01

---
+++ js/day-14/script.js
@@ -0,0 +1,78 @@
+const a=[10,20,"hjgg",30,90];
+// for(let i=0;i<a.length;i++){
+//     console.log(i);
+// }
+
+// for(let g in a){
+//     console.log(g);
+// }
+
+
+// for(let g of a){
+//     console.log(g);
+// }
+
+// function MyFun(e){
+//     console.log(e);
+// }
+
+// a.map(MyFun)
+
+
+// a.map((e)=>{
+//     console.log(e);
+// });
+
+
+// const b=a.map((e)=>{
+//     return e/2;
+// })
+
+// const b=a.filter((a)=>{
+//     return a>20;
+// })
+
+
+// const c=a.find((a)=>{
+//     return a>20;
+// })
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// const users=["Hari","Ajay","Arun","Siva"];
+
+
+// const result=users.filter((e)=>{
+
+//     return e.startsWith('si');    
+// });
+
+// console.log(result);
+
+
+// const b=a.reduce((e,i)=>{
+//     return e+i;
+// })
+let b="";
+
+// a.forEach((e)=>{
+//     b+=e;
+// })
+// console.log(b);
+
+// console.log(b);
+
+
+let sum=[7,2,4,1];
+
+
+
+    let res=sum.reduce((e,i)=>{
+        return e+i;
+    })
+
+    console.log(res);
+    
+
